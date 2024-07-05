cf009aa076d6280a71c2d5ad4aa9841178deb000
Taebae00 <161007347+Taebae00@users.noreply.github.com>
2024-07-05 17:58:12 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int j : ingredient) {
-            sb.append(j);
-            if (sb.length() > 3 && sb.subSequence(sb.length() - 4, sb.length()).equals("1231")) {
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
                 answer++;
-                sb.delete(sb.length() - 4, sb.length());
             }
         }
         return answer;
